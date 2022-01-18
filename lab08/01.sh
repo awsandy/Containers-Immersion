35282c73bf61ea485f66f784cdd291e8fa886d88
awsandy <awsandy@amazon.com>
2022-01-18 15:47:22 +0000

    up

--- lab08/01.sh
+++ lab08/01.sh
@@ -1,4 +1,4 @@
-echo "edit ~/environment/amazon-ecs-mythicalmysfits-workshop/workshop-1/app/monolith-service/service/mythicalMysfitsService.py"
+echo "editing/replacing mythicalMysfitsService.py"
 mv ~/environment/amazon-ecs-mythicalmysfits-workshop/workshop-1/app/monolith-service/service/mythicalMysfitsService.py \
 ~/environment/amazon-ecs-mythicalmysfits-workshop/workshop-1/app/monolith-service/service/mythicalMysfitsService.py.orig
 cp mythicalMysfitsService.py ~/environment/amazon-ecs-mythicalmysfits-workshop/workshop-1/app/monolith-service/
