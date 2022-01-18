ddbde81a1ce7b88a694eff8d98fd8abee4247a99
awsandy <awsandy@amazon.com>
2022-01-18 15:04:37 +0000

    up

--- lab07/05.sh
+++ lab07/05.sh
@@ -1,6 +1,6 @@
 BUCKET_NAME=$(jq < ~/environment/amazon-ecs-mythicalmysfits-workshop/workshop-1/cfn-output.json -r '.SiteBucket')
 echo "bucket-name=${BUCKET_NAME}"
 aws s3 ls ${BUCKET_NAME}
-aws s3 cp index.html s3://${BUCKET_NAME}/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers full=emailaddress=user@example.com
-aws s3 cp index.html s3://${BUCKET_NAME}/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+aws s3 cp ~/environment/amazon-ecs-mythicalmysfits-workshop/workshop-1/web/index.html s3://${BUCKET_NAME}/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers full=emailaddress=user@example.com
+#aws s3 cp index.html s3://${BUCKET_NAME}/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
 echo "hit http://${BUCKET_NAME}.s3-website.${AWS_REGION}.amazonaws.com/"
