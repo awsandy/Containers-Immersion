b9a6da4908ca04a290dd9f67a6d2e52dc035b61c
awsandy <awsandy@amazon.com>
2022-01-20 10:39:07 +0000

    up

--- lab08/05-services.sh
+++ lab08/05-services.sh
@@ -1,6 +1,6 @@
 kubectl get ing mythical-mysfits-eks -o json | jq -r '.status.loadBalancer.ingress[0].hostname'
 echo "copying /home/ec2-user/environment/amazon-ecs-mythicalmysfits-workshop/workshop-1/web/ndex.html"
-cp /home/ec2-user/environment/amazon-ecs-mythicalmysfits-workshop/workshop-1/web/ndex.html .
+cp /home/ec2-user/environment/amazon-ecs-mythicalmysfits-workshop/workshop-1/web/index.html .
 echo "edit index.html"
 aws s3 cp index.html s3://${BUCKET_NAME}/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
-echo "edit  likeservice-app.yaml "
+echo "edit  likeservice-app.yaml - update ALB name"
