6748a4ec26429532b0b1ddbf4caf5078ac740c33
awsandy <awsandy@amazon.com>
2022-01-28 14:03:46 +0000

    up

--- lab07/03-elb.sh
+++ lab07/03-elb.sh
@@ -1,2 +1,7 @@
 export ELB=$(kubectl get service mythical-mysfits-eks -o json | jq -r '.status.loadBalancer.ingress[].hostname')
 curl -m3 -v $ELB
+while [[ $? -ne 0 ]];do
+    sleep 15
+    curl -m3 -v $ELBs
+done
+echo "elb ok"
