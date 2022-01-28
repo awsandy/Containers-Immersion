b18a1a875369c3a1f34ec51b22f1740669035313
awsandy <awsandy@amazon.com>
2022-01-28 14:47:16 +0000

    up

--- lab07/05.sh
+++ lab07/05.sh
@@ -4,7 +4,7 @@ aws s3 ls ${BUCKET_NAME}
 aws s3 cp index.html s3://${BUCKET_NAME}/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers full=emailaddress=user@example.com
 #aws s3 cp index.html s3://${BUCKET_NAME}/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
 echo "hit http://${BUCKET_NAME}.s3-website.${AWS_REGION}.amazonaws.com/"
-curl http://${BUCKET_NAME}.s3-website.${AWS_REGION}.amazonaws.com/ | grep mysfits
+curl -s http://${BUCKET_NAME}.s3-website.${AWS_REGION}.amazonaws.com/ | grep mysfits > /dev/null   
 if [[ $? -eq 0 ]];then
 echo "All ok"
 fi
