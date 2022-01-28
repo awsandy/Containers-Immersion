223986f815f3d8ef2acd9dcae9882a2b97ee780b
awsandy <awsandy@amazon.com>
2022-01-28 13:30:32 +0000

    up

--- 02-Docker-stuff/01-docker.sh
+++ 02-Docker-stuff/01-docker.sh
@@ -1,7 +1,11 @@
 cp Dockerfile ~/environment/amazon-ecs-mythicalmysfits-workshop/workshop-1/app/monolith-service/
 cd ~/environment/amazon-ecs-mythicalmysfits-workshop/workshop-1/app/monolith-service/
 docker build -t monolith-service .
-
+aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 018953581346.dkr.ecr.eu-west-1.amazonaws.com
 MONO_ECR_REPOSITORY_URI=$(aws ecr describe-repositories | jq -r .repositories[].repositoryUri | grep mono)
+repo=$(echo $MONO_ECR_REPOSITORY_URI | cut -f1 -d'/')
+echo $MONO_ECR_REPOSITORY_URI
+echo $repo
+aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin $repo
 docker tag monolith-service:latest $MONO_ECR_REPOSITORY_URI:latest
 docker push $MONO_ECR_REPOSITORY_URI:latest
