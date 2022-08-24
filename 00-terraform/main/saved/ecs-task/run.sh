dad7385941871cad41203b6d17212b512a822475
awsandy <awsandy@amazon.com>
2022-08-24 13:18:33 +0100

    up

--- 00-terraform/main/saved/ecs-task/run.sh
+++ 00-terraform/main/saved/ecs-task/run.sh
@@ -1,3 +1,3 @@
-lgn=$(aws logs describe-log-groups --query logGroups[].logGroupName | jq -r .)
+lgn=$(aws logs describe-log-groups --query logGroups[].logGroupName | jq -r .[])
 echo $lgn
 export TV_VAR_lgn=$(echo $lgn)
