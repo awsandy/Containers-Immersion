873a5b97f2d3f133dc8d5f9536e697a7d59831ed
awsandy <awsandy@amazon.com>
2022-08-24 13:55:40 +0100

    up

--- 00-terraform/main/saved/ecs-task/run.sh
+++ 00-terraform/main/saved/ecs-task/run.sh
@@ -1,8 +1,10 @@
-TF_VAR_lgn=$(aws logs describe-log-groups --query logGroups[].logGroupName | jq -r .[])
-TF_VAR_etr=$(aws iam list-roles | jq -r '.Roles[] | select(.RoleName | contains("ECSTaskRole")).RoleName')
-TF_VAR_esr=$(aws iam list-roles | jq -r '.Roles[] | select(.RoleName | contains("EcsServiceRole")).RoleName')
-TF_VAR_lb=$(aws elbv2 describe-load-balancers --query LoadBalancers[].DNSName | jq -r .[])
+export TF_VAR_lgn=$(aws logs describe-log-groups --query logGroups[].logGroupName | jq -r .[])
+export TF_VAR_etr=$(aws iam list-roles | jq -r '.Roles[] | select(.RoleName | contains("ECSTaskRole")).RoleName')
+export TF_VAR_esr=$(aws iam list-roles | jq -r '.Roles[] | select(.RoleName | contains("EcsServiceRole")).RoleName')
+export TF_VAR_lb=$(aws elbv2 describe-load-balancers --query LoadBalancers[].DNSName | jq -r .[])
 echo $TF_VAR_lgn
 echo $TF_VAR_etr
 echo $TF_VAR_esr
 echo $TF_VAR_lb
+#terraform validate
+#terraform plan -out tfplan
