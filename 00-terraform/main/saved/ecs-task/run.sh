4ac97f88fb8e3af35f4e879d296917ed573101a0
awsandy <awsandy@amazon.com>
2022-08-24 13:51:13 +0100

    up

--- 00-terraform/main/saved/ecs-task/run.sh
+++ 00-terraform/main/saved/ecs-task/run.sh
@@ -1,6 +1,6 @@
 TF_VAR_lgn=$(aws logs describe-log-groups --query logGroups[].logGroupName | jq -r .[])
 TF_VAR_etr=$(aws iam list-roles | jq -r '.Roles[] | select(.RoleName | contains("ECSTaskRole")).RoleName')
-TF_VAR_esr=$(aws iam list-roles | jq -r '.Roles[] | select(.RoleName | contains("ECSServiceRole")).RoleName')
+TF_VAR_esr=$(aws iam list-roles | jq -r '.Roles[] | select(.RoleName | contains("EcsServiceRole")).RoleName')
 TF_VAR_lb=$(aws elbv2 describe-load-balancers --query LoadBalancers[].DNSName | jq -r .[])
 echo $TF_VAR_lgn
 echo $TF_VAR_etr
