f7e4a76a0a5d72cccfdfd6746c3f6a37bb92124f
awsandy <awsandy@amazon.com>
2022-08-24 16:18:23 +0100

    up

--- 00-terraform/main/saved/ecs-task/run-task.sh
+++ 00-terraform/main/saved/ecs-task/run-task.sh
@@ -1,7 +1,10 @@
 export TF_VAR_cn=$(aws ecs list-clusters | jq -r '. | select(.clusterArns[] | contains("Cluster-mod-")).clusterArns' | jq -r .[0] | rev | cut -f1 -d'/' | rev)
 
-tdarn=$(terraform show -json | jq '.values.root_module.resources[] | select (.address=="aws_ecs_task_definition.task-definition_Monolith-Definition-mod-c24aacc7ec26455c_1").values.arn')
-
+tdarn=$(terraform show -json | jq -r '.values.root_module.resources[] | select (.address=="aws_ecs_task_definition.task-definition_Monolith-Definition-mod-c24aacc7ec26455c_1").values.arn')
+vpcid=$(aws ec2 describe-vpcs --filters "Name=is-default,Values=false" --query Vpcs[].VpcId | jq -r .[])
+sub1=$(aws ec2 describe-subnets --filters "Name=vpc-id,Values=$vpcid" | jq '.Subnets[] |  select(.MapPublicIpOnLaunch==true)' | jq -r .SubnetId | head -1)
+echo $sub1
 aws ecs run-task --cluster $TF_VAR_cn \
     --task-definition $tdarn \
-    --launch-type FARGATE 
+    --launch-type FARGATE \
+    --network-configuration awsvpcConfiguration={subnets=["$sub1"],assignPublicIp="ENABLED"}
