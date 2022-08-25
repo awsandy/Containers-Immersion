5824bc527e838f24effce07ea6bd326d066009af
awsandy <awsandy@amazon.com>
2022-08-25 10:16:47 +0100

    up

---
+++ 00-setup/02-awscli-v2.sh
@@ -0,0 +1,15 @@
+echo "Install OS tools"
+sudo yum -y -q -e 0 install  jq moreutils nmap > /dev/null
+echo "Update OS tools"
+sudo yum update -y > /dev/null
+echo "Update pip"
+sudo pip install --upgrade pip 2&> /dev/null
+echo "Uninstall AWS CLI v1"
+sudo /usr/local/bin/pip uninstall awscli -y 2&> /dev/null
+sudo pip uninstall awscli -y 2&> /dev/null
+echo "Install AWS CLI v2"
+curl --silent "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" > /dev/null
+unzip -qq awscliv2.zip
+sudo ./aws/install > /dev/null
+rm -f awscliv2.zip
+rm -rf aws
