fb9e40a57b1366155b416cb2a39307231262700f
awsandy <awsandy@amazon.com>
2022-08-26 13:46:52 +0100

    1

--- 02-lab02-ecs-task/04-check-task.sh
+++ 02-lab02-ecs-task/04-check-task.sh
@@ -4,11 +4,12 @@ export TF_VAR_muid=$(echo $TF_VAR_lgn | cut -f2 -d'-')
 export TF_VAR_lb=$(aws elbv2 describe-load-balancers --query LoadBalancers[].DNSName | jq -r .[])
 echo $TF_VAR_lb
 aws ecs list-tasks --cluster $TF_VAR_cn --family Monolith-Definition-mod-${TF_VAR_muid}
-curl $TF_VAR_lb/mysfits | grep haetae
+curl $TF_VAR_lb/mysfits | grep haetae > /dev/null
 while [[ $? -ne 0 ]]; do
 echo "not ready sleep 15s .."
 sleep 15
-curl $TF_VAR_lb/mysfits | grep haetae
+curl $TF_VAR_lb/mysfits | grep haetae > /dev/null
 
 done
 echo "success"
+curl $TF_VAR_lb/mysfits
