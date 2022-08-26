10f9af4d63c0f54a424c5d62c24ed550f7a9d17e
awsandy <awsandy@amazon.com>
2022-08-26 13:48:39 +0100

    up

--- 02-lab02-ecs-task/04-check-task.sh
+++ 02-lab02-ecs-task/04-check-task.sh
@@ -3,7 +3,7 @@ export TF_VAR_lgn=$(aws logs describe-log-groups --query logGroups[].logGroupNam
 export TF_VAR_muid=$(echo $TF_VAR_lgn | cut -f2 -d'-')
 export TF_VAR_lb=$(aws elbv2 describe-load-balancers --query LoadBalancers[].DNSName | jq -r .[])
 echo $TF_VAR_lb
-aws ecs list-tasks --cluster $TF_VAR_cn --family Monolith-Definition-mod-${TF_VAR_muid}
+#aws ecs list-tasks --cluster $TF_VAR_cn --family Monolith-Definition-mod-${TF_VAR_muid}
 curl -s $TF_VAR_lb/mysfits | grep haetae > /dev/null
 while [[ $? -ne 0 ]]; do
 echo "not ready sleep 15s .."
