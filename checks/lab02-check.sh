e7d30ae2319aeab63bc79e1c2d7b8524249b2201
awsandy <awsandy@amazon.co.uk>
2023-08-20 12:43:30 +0100

    up

--- checks/lab02-check.sh
+++ checks/lab02-check.sh
@@ -6,5 +6,12 @@ else
     echo "ERROR: no task definition for mono"
 fi
 # image check
-td=$(aws ecs list-task-definitions --query taskDefinitionArns --output text | grep containersid | grep -i mono)
-aws ecs describe-task-definition --task-definition $td 
+aws ecs list-task-definitions --query taskDefinitionArns --output text | grep containersid | grep -i mono | grep ':2' >/dev/null
+if [[ $? -ne 0 ]]; then
+    echo "ERROR: expected second :2 task definition"
+fi
+td=$(aws ecs list-task-definitions --query taskDefinitionArns | jq .[] |  aws ecs list-task-definitions --query taskDefinitionArns | jq -r '.[]' | grep ':2')
+aws ecs describe-task-definition --task-definition $td --query taskDefinition.containerDefinitions | grep image | grep containersid-mono > /dev/null
+if [[ $? -ne 0 ]]; then
+    echo "ERROR: no containersid-mono ECR image in modified task definition"
+fi
