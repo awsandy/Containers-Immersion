a1c7a214abc7a6e2bb1ab64b5e32a8f0423859bd
awsandy <awsandy@amazon.co.uk>
2023-08-20 11:45:44 +0100

    up

--- checks/lab01-check.sh
+++ checks/lab01-check.sh
@@ -4,3 +4,15 @@ if [[ $rc -eq 2 ]]; then
 else
     echo "ERROR: found $rc images in ECR repo - expected 2"
 fi
+aws ecr describe-repositories | jq .repositories[].repositoryName | grep containersid-mono > /dev/null
+if [[ $? -ne 0 ]]; then
+    echo "ERROR: Can't find mono image in ECR"
+else
+    echo "PASSED: mono image in ECR "  
+fi
+aws ecr describe-repositories | jq .repositories[].repositoryName | grep containersid-like  > /dev/null
+if [[ $? -ne 0 ]]; then
+    echo "ERROR: Can't find like image in ECR"
+else
+    echo "PASSED: like image in ECR "  
+fi
