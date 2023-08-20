40d0a2f323bc56bb2a440fb13dec42cd5403face
awsandy <awsandy@amazon.co.uk>
2023-08-20 20:39:06 +0100

    up

--- 04-microservices/02-docker-build.sh
+++ 04-microservices/02-docker-build.sh
@@ -7,9 +7,11 @@ aws ecr get-login-password --region eu-west-1 | docker login --username AWS --pa
 docker build -t monolith-service:nolike .
 docker tag monolith-service:nolike $MONO_ECR_REPOSITORY_URI:nolike
 docker push $MONO_ECR_REPOSITORY_URI:nolike
+echo "pushed mono service image - with nolike tag"
 #
 cd ~/environment/amazon-ecs-mythicalmysfits-workshop/workshop-1/app/like-service
 LIKE_ECR_REPOSITORY_URI=$(aws ecr describe-repositories | jq -r .repositories[].repositoryUri | grep like)
 docker build -t like-service .
 docker tag like-service:latest $LIKE_ECR_REPOSITORY_URI:latest
 docker push $LIKE_ECR_REPOSITORY_URI:latest
+echo "pushed like service image"
---
+++ checks/lab04a-check.sh
@@ -0,0 +1,21 @@
+echo "check edits"
+grep 'app.route("/mysfits/<mysfit_id>/fulfill-like' ~/environment/amazon-ecs-mythicalmysfits-workshop/workshop-1/app/monolith-service/service/mythicalMysfitsService.py | grep '#'
+if [[ $? -eq 0 ]]; then
+    echo "ERROR: app.route fullfill-like still commented in mythicalMysfitsService.py"
+else
+    echo "PASSED: app.route fullfill-like uncommented in mythicalMysfitsService.py"
+fi
+## check monolyth now has nolike tag - as like now implemented elsewhere
+aws ecr describe-repositories | jq .repositories[].repositoryName | grep containersid-mono > /dev/null
+if [[ $? -ne 0 ]]; then
+    echo "ERROR: Can't find mono image in ECR"
+else
+    echo "PASSED: mono image in ECR "  
+fi
+rn=$(aws ecr describe-repositories | jq -r .repositories[].repositoryName | grep containersid-mono)
+aws ecr describe-images --repository-name $rn | grep nolike 
+if [[ $? -ne 0 ]];then
+    echo "ERROR: Can't find mono image nolike tag in ECR"
+else
+    echo "PASSED: mono image nolike tag found in ECR " 
+fi
