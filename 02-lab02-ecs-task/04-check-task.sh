dad1276ae3ee2ffd293cec7142284facf5c4fe0c
awsandy <awsandy@amazon.co.uk>
2023-08-20 13:13:08 +0100

    up

--- 02-lab02-ecs-task/04-check-task.sh
+++ 02-lab02-ecs-task/04-check-task.sh
@@ -1,4 +1,4 @@
-export TF_VAR_cn=$(aws ecs list-clusters | jq -r '. | select(.clusterArns[] | contains("Cluster-mod-")).clusterArns' | jq -r .[0] | rev | cut -f1 -d'/' | rev)
+export TF_VAR_cn=$(aws ecs list-clusters | jq -r '. | select(.clusterArns[] | contains("containersid")).clusterArns' | jq -r .[0] | rev | cut -f1 -d'/' | rev)
 export TF_VAR_lgn=$(aws logs describe-log-groups --query logGroups[].logGroupName | jq -r .[])
 export TF_VAR_muid=$(echo $TF_VAR_lgn | cut -f2 -d'-')
 export TF_VAR_lb=$(aws elbv2 describe-load-balancers --query LoadBalancers[].DNSName | jq -r .[])
