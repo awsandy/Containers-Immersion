39189c7ec723c5103f042d6175b55f64974ae195
awsandy <awsandy@amazon.co.uk>
2023-08-20 12:31:16 +0100

    up

--- 02-lab02-ecs-task/00-task-definition.sh
+++ 02-lab02-ecs-task/00-task-definition.sh
@@ -1,4 +1,4 @@
-export TF_VAR_lgn=$(aws logs describe-log-groups --query logGroups[].logGroupName | jq -r .[])
+export TF_VAR_lgn=$(aws logs describe-log-groups --query logGroups[].logGroupName | jq -r .[] | grep -i mono )
 export TF_VAR_etr=$(aws iam list-roles | jq -r '.Roles[] | select(.RoleName | contains("ECSTaskRole")).Arn')
 export TF_VAR_esr=$(aws iam list-roles | jq -r '.Roles[] | select(.RoleName | contains("EcsServiceRole")).Arn')
 export TF_VAR_lb=$(aws elbv2 describe-load-balancers --query LoadBalancers[].DNSName | jq -r .[])
