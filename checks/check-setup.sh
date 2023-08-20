b2db5fd1c2b4e0eed855499c4e44dc64136e15be
awsandy <awsandy@amazon.co.uk>
2023-08-20 10:35:46 +0100

    up

--- checks/check-setup.sh
+++ checks/check-setup.sh
@@ -7,27 +7,27 @@ else
 fi
 ###
 echo "checking base environment vars"
-echo $BUCKET_NAME | grep containersid-mythicalbucket
+echo $BUCKET_NAME | grep containersid-mythicalbucket > /dev/null
 if [[ $? -ne 0 ]]; then
     echo "ERROR: BUCKET_NAME not set / incorrect = $BUCKET_NAME"
 else
-    echo "PASSED: BUCKET_NAME correctly = $BUCKET_NAME"
+    echo "PASSED: BUCKET_NAME = $BUCKET_NAME"
 fi
 echo $TABLE_NAME | grep containersid
 if [[ $? -ne 0 ]]; then
     echo "ERROR: TABLE_NAME not set / incorrect = $TABLE_NAME"
 else
-    echo "PASSED: TABLE_NAME correctly = $TABLE_NAME"
+    echo "PASSED: TABLE_NAME = $TABLE_NAME"
 fi
 echo $API_ENDPOINT | grep containersid
 if [[ $? -ne 0 ]]; then
     echo "ERROR: API_ENDPOINT not set / incorrect = $API_ENDPOINT"
 else
-    echo "PASSED: API_ENDPOINT correctly = $API_ENDPOINT"
+    echo "PASSED: API_ENDPOINT = $API_ENDPOINT"
 fi
 echo $MONO_ECR_REPOSITORY_URI | grep containersid
 if [[ $? -ne 0 ]]; then
     echo "ERROR: MONO_ECR_REPOSITORY_URI not set / incorrect = $MONO_ECR_REPOSITORY_URI"
 else
-    echo "PASSED: MONO_ECR_REPOSITORY_URI correctly = $MONO_ECR_REPOSITORY_URI"
+    echo "PASSED: MONO_ECR_REPOSITORY_URI = $MONO_ECR_REPOSITORY_URI"
 fi
