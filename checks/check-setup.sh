00d1079a883d62ba39aef6df7e96dfb93c760e74
awsandy <awsandy@amazon.co.uk>
2023-08-20 10:20:32 +0100

    up

--- checks/check-setup.sh
+++ checks/check-setup.sh
@@ -1,7 +1,6 @@
 reg=$(aws configure get region)
 n=${#reg}
 echo $n
-if [[ $n .lt 0 ]];then
-echo "region set incorrectly $reg"
-exit
+if [[ $n -lt 0 ]];then
+echo "ERROR: region set incorrectly $reg"
 fi
