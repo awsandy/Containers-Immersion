fc4bcb2308cc0df7f0be3266ecac4716dca45082
awsandy <awsandy@amazon.co.uk>
2023-08-21 12:08:21 +0100

    up

--- 04-microservices/04-task-definition-like.sh
+++ 04-microservices/04-task-definition-like.sh
@@ -2,8 +2,8 @@ export TF_VAR_lgn=$(aws logs describe-log-groups --query logGroups[].logGroupNam
 export TF_VAR_etr=$(aws iam list-roles | jq -r '.Roles[] | select(.RoleName | contains("ECSTaskRole")).Arn')
 export TF_VAR_esr=$(aws iam list-roles | jq -r '.Roles[] | select(.RoleName | contains("EcsServiceRole")).Arn')
 export TF_VAR_lb=$(aws elbv2 describe-load-balancers --query LoadBalancers[].DNSName | jq -r .[])
-export TF_VAR_tn=$(aws dynamodb list-tables | jq -r '. | select(.TableNames[] | contains("Table-mod-")).TableNames' | jq -r .[0])
-export TF_VAR_cn=$(aws ecs list-clusters | jq -r '. | select(.clusterArns[] | contains("Cluster-mod-")).clusterArns' | jq -r .[0] | rev | cut -f1 -d'/' | rev)
+export TF_VAR_tn=$(aws dynamodb list-tables | jq -r '. | select(.TableNames[] | contains("containersid")).TableNames' | jq -r .[0])
+export TF_VAR_cn=$(aws ecs list-clusters | jq -r '. | select(.clusterArns[] | contains("containersid")).clusterArns' | jq -r .[0] | rev | cut -f1 -d'/' | rev)
 export TF_VAR_sn=$(aws ecs list-services --cluster $TF_VAR_cn | jq -r '. | select(.serviceArns[] | contains("MythicalMonolithService")).serviceArns' | jq -r .[0] | rev | cut -f1 -d'/' | rev)
 export TF_VAR_ruri=$(aws ecr describe-repositories | jq -r .repositories[].repositoryUri | grep mono)
 export TF_VAR_muid=$(echo $TF_VAR_lgn | cut -f2 -d'-')
@@ -11,7 +11,7 @@ export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
 export AWS_REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region')
 export TF_VAR_luri=$(aws ecr describe-repositories | jq -r .repositories[].repositoryUri | grep like)
 
-
+echo "########"
 echo $TF_VAR_lgn
 echo $TF_VAR_etr
 echo $TF_VAR_esr
@@ -20,6 +20,7 @@ echo $TF_VAR_tn
 echo $TF_VAR_ruri
 echo $TF_VAR_cn
 echo $TF_VAR_sn
+echo "########"
 echo muid=$TF_VAR_muid
 
 
@@ -57,7 +58,7 @@ cat << EOF > like-container.json
 EOF
 
 
-aws ecs register-task-definition --family Like-Definition-mod-${TF_VAR_muid} --network-mode awsvpc \
+aws ecs register-task-definition --family containersid-like-${TF_VAR_muid} --network-mode awsvpc \
 --execution-role-arn ${TF_VAR_esr} \
 --requires-compatibilities FARGATE \
 --cpu 256 \
--- 04-microservices/05-update-service.sh
+++ 04-microservices/05-update-service.sh
@@ -1,4 +1,4 @@
-export TF_VAR_cn=$(aws ecs list-clusters | jq -r '. | select(.clusterArns[] | contains("Cluster-mod-")).clusterArns' | jq -r .[0] | rev | cut -f1 -d'/' | rev)
+export TF_VAR_cn=$(aws ecs list-clusters | jq -r '. | select(.clusterArns[] | contains("containersid")).clusterArns' | jq -r .[0] | rev | cut -f1 -d'/' | rev)
 export TF_VAR_sn=$(aws ecs list-services --cluster $TF_VAR_cn | jq -r '. | select(.serviceArns[] | contains("MythicalMonolithService")).serviceArns' | jq -r .[0] | rev | cut -f1 -d'/' | rev)
 export TF_VAR_ruri=$(aws ecr describe-repositories | jq -r .repositories[].repositoryUri | grep mono)
 export TF_VAR_lgn=$(aws logs describe-log-groups --query logGroups[].logGroupName | jq -r .[])
