facb3b4ea528bfc5cf1cbdc95f5cae3929f2f6bb
awsandy <awsandy@amazon.co.uk>
2023-08-21 12:46:29 +0100

    up

--- 04-microservices/05-update-service.sh
+++ 04-microservices/05-update-service.sh
@@ -3,9 +3,9 @@ export TF_VAR_sn=$(aws ecs list-services --cluster $TF_VAR_cn | jq -r '. | selec
 export TF_VAR_ruri=$(aws ecr describe-repositories | jq -r .repositories[].repositoryUri | grep mono)
 export TF_VAR_lgn=$(aws logs describe-log-groups --query logGroups[].logGroupName | jq -r .[])
 export TF_VAR_muid=$(echo $TF_VAR_lgn | cut -f2 -d'-')
-tdarn=$(aws ecs list-task-definitions --query taskDefinitionArns | jq -r .[] | grep Monolith-Definition-mod- | tail -1)
+tdarn=$(aws ecs list-task-definitions --query taskDefinitionArns | jq -r .[] | grep Monolith-Definition | tail -1)
 export TF_VAR_lb=$(aws elbv2 describe-load-balancers --query LoadBalancers[].DNSName | jq -r .[])
-
+echo $tdarn
 # update monlyth service
 aws ecs update-service --cluster $TF_VAR_cn --service $TF_VAR_sn --task-definition $tdarn
 
