fca40d487057293d11792f954e1aea8e113933cf
awsandy <awsandy@amazon.co.uk>
2023-08-29 10:47:25 +0100

    up

---
+++ checks/lab07-check..sh
@@ -0,0 +1,8 @@
+grep '${' ~/environment/amazon-ecs-mythicalmysfits-workshop/workshop-1/app/monolith-service/monolith-app.yaml
+if [[ $? -eq 0 ]];then
+    echo "ERROR: check env vars sub in ~/environment/amazon-ecs-mythicalmysfits-workshop/workshop-1/app/monolith-service/monolith-app.yaml"
+else
+    echo "PASSED: check env vars sub in ~/environment/amazon-ecs-mythicalmysfits-workshop/workshop-1/app/monolith-service/monolith-app.yaml"
+fi
+echo "Repo check"
+./lab01-check.sh
