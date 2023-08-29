e05b730313567a30ddb7bbace46339db7918be5c
awsandy <awsandy@amazon.co.uk>
2023-08-29 10:35:02 +0100

    up

--- checks/lab05-check.sh
+++ checks/lab05-check.sh
@@ -1,12 +1,25 @@
 grep '${' ~/environment/mythicaleks.yaml
-if [[ $? -eq 1 ]];then
+if [[ $? -eq 0 ]];then
     echo "ERROR: check env vars sub in ~/environment/mythicaleks.yaml"
+else
+    echo "PASSED: check env vars sub in ~/environment/mythicaleks.yaml"
 fi
-nc=$(kubectl get nodes)
+reg=$(aws configure get region)
+grep $reg ~/environment/mythicaleks.yaml
+if [[ $? -ne 0 ]];then
+    echo "ERROR: no region $reg in ~/environment/mythicaleks.yaml"
+else
+    echo "PASSED: region $reg in ~/environment/mythicaleks.yaml"
+fi
+nc=$(kubectl get nodes | grep Ready | wc -l )
 if [[ $nc -lt 3 ]];then 
-    echo "ERROR: expected 3x EKS nodes got $nc"
+    echo "ERROR: expected 3x Ready EKS nodes got $nc"
+else
+    echo "PASSED: 3x Ready EKS nodes got $nc"
 fi
-pc=$(kubectl get pods -n kube-system | wc -l)
+pc=$(kubectl get pods -n kube-system | grep Running | wc -l)
 if [[ $pc -lt 8 ]];then 
     echo "ERROR: expected 8x running pods in kube-system got $pc"
+else
+    echo "PASSED: 8x  running pods in kube-system got $pc"
 fi
