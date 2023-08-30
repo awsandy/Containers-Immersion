841ddc66a84eed61492d923c1fb94035bb4febb0
awsandy <awsandy@amazon.co.uk>
2023-08-30 11:08:25 +0100

    up

--- checks/lab08-check.sh
+++ checks/lab08-check.sh
@@ -14,14 +14,38 @@ fi
 ## check monolyth now has nolike tag - as like now implemented elsewhere
 aws ecr describe-repositories | jq .repositories[].repositoryName | grep containersid-mono > /dev/null
 if [[ $? -ne 0 ]]; then
-    echo "ERROR: Can't find mono image in ECR"
+    echo "ERROR: Can't find mono repo in ECR"
 else
-    echo "PASSED: mono image in ECR "  
+    echo "PASSED: mono rewpo in ECR "  
 fi
 rn=$(aws ecr describe-repositories | jq -r .repositories[].repositoryName | grep containersid-mono) 
 aws ecr describe-images --repository-name $rn | grep nolike > /dev/null
 if [[ $? -ne 0 ]];then
-    echo "ERROR: Can't find mono image nolike tag in ECR"
+    echo "ERROR: Can't find mono image nolike tag in ECR repo $rn"
 else
-    echo "PASSED: mono image nolike tag found in ECR " 
+    echo "PASSED: mono image nolike tag found in ECR repo $rn " 
 fi
+rn=$(aws ecr describe-repositories | jq -r .repositories[].repositoryName | grep containersid-like) 
+aws ecr describe-images --repository-name $rn | grep like > /dev/null
+if [[ $? -ne 0 ]];then
+    echo "ERROR: Can't find like image in ECR repo $rn"
+else
+    echo "PASSED: like image found in ECR repo $rn " 
+fi
+
+
+# check manifest /home/ec2-user/environment/amazon-ecs-mythicalmysfits-workshop/workshop-1/nolikeservice-app.yaml
+grep containersid-mono /home/ec2-user/environment/amazon-ecs-mythicalmysfits-workshop/workshop-1/nolikeservice-app.yaml | grep nolike > /dev/null
+if [[ $? -ne 0 ]];then
+    echo "ERROR: Can't find nolike image in manifest workshop-1/nolikeservice-app.yaml "
+else
+    echo "PASSED: Found nolike image in manifest workshop-1/nolikeservice-app.yaml " 
+fi
+#check manifest /home/ec2-user/environment/amazon-ecs-mythicalmysfits-workshop/workshop-1/likeservice-app.yaml
+grep containersid-like /home/ec2-user/environment/amazon-ecs-mythicalmysfits-workshop/workshop-1/likeservice-app.yaml | grep latest > /dev/null
+if [[ $? -ne 0 ]];then
+    echo "ERROR: Can't find like image in manifest workshop-1/likeservice-app.yaml "
+else
+    echo "PASSED: Found like image in manifest workshop-1/likeservice-app.yaml " 
+fi
+
