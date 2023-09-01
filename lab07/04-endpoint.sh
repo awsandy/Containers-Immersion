45707808f389e7632db7968156a881ebc7cb4511
awsandy <awsandy@amazon.co.uk>
2023-09-01 17:57:36 +0100

    up

--- lab07/04-endpoint.sh
+++ lab07/04-endpoint.sh
@@ -1,3 +1,4 @@
+ELB=$(kubectl get service mythical-mysfits-eks -o json | jq -r '.status.loadBalancer.ingress[].hostname')
 cp /home/ec2-user/environment/amazon-ecs-mythicalmysfits-workshop/workshop-1/web/index.html index.html.orig
 grep REPLACE_ME_API_ENDPOINT index.html.orig
 comm=$(printf "sed 's/REPLACE_ME_API_ENDPOINT/http:\/\/%s/' index.html.orig > index.html" $ELB)
