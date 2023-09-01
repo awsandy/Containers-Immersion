e0f5578729e77e4b1f877ac98fbff796d8d780f8
awsandy <awsandy@amazon.co.uk>
2023-09-01 13:34:50 +0100

    up

--- cleanup-all.sh
+++ cleanup-all.sh
@@ -4,3 +4,16 @@ for i in $srvs;do
 aws ecs update-service --cluster Cluster-containersid --service $i --desired-count 0 --output text
 aws ecs delete-service --cluster Cluster-containersid --service $i --output text
 done
+tsks=$(aws ecs list-tasks --cluster Cluster-containersid -query taskArns | jq -r .[] | rev | cut -f1 -d'/' | rev)
+for i in $tsks;do
+aws ecs stop-task --task $i --cluster Cluster-containersid
+done
+tds=$(aws ecs list-task-definitions | jq -r .taskDefinitionArns[] | rev | cut -f1 -d'/' | rev)
+for i in $tds;do
+if [[ $i != *"Monolith-Definition-containersid:1" ]];then
+aws ecs deregister-task-definition --task-definition $i
+fi
+done
+
+
+
--- do-all.sh
+++ do-all.sh
@@ -1,4 +1,6 @@
 cd 00-setup
+./01*
+./02*
 . ./03-key-env.sh
 cd ~/environment/Containers-Immersion
 ./setup-check.sh
