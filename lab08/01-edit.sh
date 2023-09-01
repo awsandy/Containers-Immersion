b15dcf0b3c92b069072f7fe1672dd1524a2301a4
awsandy <awsandy@amazon.co.uk>
2023-09-01 18:26:39 +0100

    up

--- lab08/01-edit.sh
+++ lab08/01-edit.sh
@@ -1,4 +1,4 @@
-
+kubectl delete deployment mythical-mysfits-eks 2> /dev/null
 echo "editing/replacing mythicalMysfitsService.py"
 mv ~/environment/amazon-ecs-mythicalmysfits-workshop/workshop-1/app/monolith-service/service/mythicalMysfitsService.py \
 ~/environment/amazon-ecs-mythicalmysfits-workshop/workshop-1/app/monolith-service/service/mythicalMysfitsService.py.orig
