83197a35c783d22af14e1b4cf32ddcb48c564ce7
awsandy <awsandy@amazon.co.uk>
2023-09-01 15:23:11 +0100

    up

--- lab05-eks-cluster/00-clean-docker.sh
+++ lab05-eks-cluster/00-clean-docker.sh
@@ -1,2 +1,10 @@
 # remove images
 (docker images -q | xargs docker rmi || true) 2> /dev/null
+instance_id=$(curl -sS http://169.254.169.254/latest/meta-data/instance-id)
+profile_name="$(jq < ~/environment/amazon-ecs-mythicalmysfits-workshop/workshop-1/cfn-output.json -r '.ProfileName')"
+
+if aws ec2 associate-iam-instance-profile --iam-instance-profile "Name=$profile_name" --instance-id $instance_id; then
+  echo "Profile $profile_name associated successfully."
+else
+  echo "WARNING: Encountered error associating instance profile with Cloud9 environment"
+fi
