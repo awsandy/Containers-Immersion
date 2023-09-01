6037d54a8bdaef54474fb07193dbb88c0c0d7692
awsandy <awsandy@amazon.co.uk>
2023-09-01 16:57:11 +0100

    up

--- lab07/03-elb.sh
+++ lab07/03-elb.sh
@@ -1,7 +1,8 @@
 export ELB=$(kubectl get service mythical-mysfits-eks -o json | jq -r '.status.loadBalancer.ingress[].hostname')
 curl -m3 -v $ELB
 while [[ $? -ne 0 ]];do
+    echo "Waiting for ELB - sleeping 15s ....."
     sleep 15
-    curl -m3 -v $ELBs
+    curl -m3 -v $ELB
 done
 echo "elb ok"
