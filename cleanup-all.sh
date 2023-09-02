387f3d25bd2ee2adbbef0ee377d1d467c27b713f
awsandy <awsandy@amazon.co.uk>
2023-09-02 16:57:48 +0100

    up

--- cleanup-all.sh
+++ cleanup-all.sh
@@ -1,34 +1,43 @@
-# scale and delete ECS services
-srvs=$(aws ecs list-services --cluster Cluster-containersid --query serviceArns | jq -r .[] | rev | cut -f1 -d'/' | rev)
-for i in $srvs;do
-aws ecs update-service --cluster Cluster-containersid --service $i --desired-count 0 --output text
-aws ecs delete-service --cluster Cluster-containersid --service $i --output text
-done
-tsks=$(aws ecs list-tasks --cluster Cluster-containersid --query taskArns | jq -r .[] | rev | cut -f1 -d'/' | rev)
-for i in $tsks;do
-aws ecs stop-task --task $i --cluster Cluster-containersid
-done
-tds=$(aws ecs list-task-definitions | jq -r .taskDefinitionArns[] | rev | cut -f1 -d'/' | rev)
-for i in $tds;do
-if [[ $i != *"Monolith-Definition-containersid:1" ]];then
-aws ecs deregister-task-definition --task-definition $i
+(docker images -q | xargs docker rmi || true) 2>/dev/null
+if [[ $1 != "eks" ]]; then
+    echo "scale ECS services to 0 and delete them"
+    srvs=$(aws ecs list-services --cluster Cluster-containersid --query serviceArns | jq -r .[] | rev | cut -f1 -d'/' | rev)
+    for i in $srvs; do
+        aws ecs update-service --cluster Cluster-containersid --service $i --desired-count 0 --output text
+        aws ecs delete-service --cluster Cluster-containersid --service $i --output text
+    done
+    echo "stop any ECS tasks"
+    tsks=$(aws ecs list-tasks --cluster Cluster-containersid --query taskArns | jq -r .[] | rev | cut -f1 -d'/' | rev)
+    for i in $tsks; do
+        aws ecs stop-task --task $i --cluster Cluster-containersid
+    done
+    echo "delete ECS task definitions - except Monolith-Definition-containersid:1 "
+    tds=$(aws ecs list-task-definitions | jq -r .taskDefinitionArns[] | rev | cut -f1 -d'/' | rev)
+    for i in $tds; do
+        if [[ $i != *"Monolith-Definition-containersid:1" ]]; then
+            aws ecs deregister-task-definition --task-definition $i
+        fi
+    done
 fi
-done
+echo "delete EKS manifestes etc.."
+kubectl delete deployment mythical-mysfits-like
+kubectl delete deployment mythical-mysfits-nolike
+kubectl get ingress/mythical-mysfits-eks
 echo "delete EKS addons"
 eksctl delete iamserviceaccount \
-  --cluster=mythicaleks-eksctl \
-  --namespace=kube-system \
-  --name=aws-load-balancer-controller
+    --cluster=mythicaleks-eksctl \
+    --namespace=kube-system \
+    --name=aws-load-balancer-controller
 eksctl create iamserviceaccount \
-		--cluster=mythicaleks-eksctl \
-		--namespace=default \
-		--name=mythical-misfit
+    --cluster=mythicaleks-eksctl \
+    --namespace=default \
+    --name=mythical-misfit
 echo "delete EKS nodegroup"
 eksctl delete nodegroup --cluster mythicaleks-eksctl --name nodegroup || true
 echo "Waiting for stack set eksctl-mythicaleks-eksctl-nodegroup-nodegroup"
-aws cloudformation wait stack-delete-complete  --stack-name eksctl-mythicaleks-eksctl-nodegroup-nodegroup
+aws cloudformation wait stack-delete-complete --stack-name eksctl-mythicaleks-eksctl-nodegroup-nodegroup
 echo "delete EKS cluster"
 eksctl delete cluster --name mythicaleks-eksctl || true
 echo "Waiting for stack set eksctl-mythicaleks-eksctl"
-aws cloudformation wait stack-delete-complete  --stack-name eksctl-mythicaleks-eksctl
-(docker images -q | xargs docker rmi || true) 2> /dev/null
+aws cloudformation wait stack-delete-complete --stack-name eksctl-mythicaleks-eksctl
+
