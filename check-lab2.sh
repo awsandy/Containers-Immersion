167c297e630629b6b33bb060f5ca9a4c109bda1f
awsandy <awsandy@amazon.co.uk>
2023-09-04 13:46:07 +0100

    up

--- check-lab2.sh
+++ check-lab2.sh
@@ -6,11 +6,11 @@ else
     echo "PASSED: found $rc task definitions for Mono"
 fi
 # image check
-aws ecs list-task-definitions --query taskDefinitionArns --output text | grep containersid | grep -i mono | grep ':2' >/dev/null
-if [[ $? -ne 0 ]]; then
-    echo "ERROR: expected second :2 task definition"
+tdc=$(aws ecs list-task-definitions --query taskDefinitionArns --output text | grep containersid | grep -i mono | wc -l 
+if [[ $tdc -lt 2 ]]; then
+    echo "ERROR: expected second task definition"
 else
-    echo "PASSED: found second :2 task definition"
+    echo "PASSED: found >1 task definition for Mono"
 fi
 td=$(aws ecs list-task-definitions --query taskDefinitionArns | jq .[] |  aws ecs list-task-definitions --query taskDefinitionArns | jq -r '.[]' | grep ':2')
 aws ecs describe-task-definition --task-definition $td --query taskDefinition.containerDefinitions | grep image | grep containersid-mono > /dev/null
--- reset-lab2.sh
+++ reset-lab2.sh
@@ -1,12 +1,12 @@
 echo "stop any ECS tasks"
 tsks=$(aws ecs list-tasks --cluster Cluster-containersid --query taskArns | jq -r .[] | rev | cut -f1 -d'/' | rev)
 for i in $tsks; do
-    aws ecs stop-task --task $i --cluster Cluster-containersid
+    aws ecs stop-task --task $i --cluster Cluster-containersid --output text
 done
 echo "delete ECS task definitions - except Monolith-Definition-containersid:1 "
 tds=$(aws ecs list-task-definitions | jq -r .taskDefinitionArns[] | rev | cut -f1 -d'/' | rev)
 for i in $tds; do
     if [[ $i != *"Monolith-Definition-containersid:1" ]]; then
-        aws ecs deregister-task-definition --task-definition $i
+        aws ecs deregister-task-definition --task-definition $i --output text
     fi
 done
