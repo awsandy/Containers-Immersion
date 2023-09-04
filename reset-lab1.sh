e2b24ab375ae8afd7d3ec7753d7a3440642100f5
awsandy <awsandy@amazon.co.uk>
2023-09-04 12:16:46 +0100

    up

--- reset-lab1.sh
+++ reset-lab1.sh
@@ -3,4 +3,6 @@ repo=$(echo $MONO_ECR_REPOSITORY_URI | cut -f1 -d'/')
 echo $MONO_ECR_REPOSITORY_URI
 echo $repo
 aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin $repo
-docker rm $MONO_ECR_REPOSITORY_URI:latest
+docker rmi $MONO_ECR_REPOSITORY_URI:latest
+docker rmi monolith-service:latest
+docker rmi ubuntu:20.04
