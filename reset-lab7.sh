6d57217d8a47b1576f6b41d6f506fef3fecf87f9
awsandy <awsandy@amazon.co.uk>
2023-09-06 22:29:49 +0100

    up

--- reset-lab7.sh
+++ reset-lab7.sh
@@ -1,5 +1,6 @@
-kubectl delete deployment mythical-mysfits-eks 2> /dev/null
-kubectl delete service mythical-mysfits-eks 2> /dev/null
+#kubectl delete deployment mythical-mysfits-eks 2> /dev/null
+#kubectl delete service mythical-mysfits-eks 2> /dev/null
+kubectl delete -f /home/ec2-user/environment/amazon-ecs-mythicalmysfits-workshop/workshop-1/app/monolith-service/monolith-app.yaml
 #eksctl delete iamserviceaccount \
 #		--cluster=mythicaleks-eksctl \
 #		--namespace=default \
