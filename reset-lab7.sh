9d14bb2e1491830ed539b743fa8705d3641f1341
awsandy <awsandy@amazon.co.uk>
2023-09-06 22:26:01 +0100

    up

--- reset-lab7.sh
+++ reset-lab7.sh
@@ -1,9 +1,9 @@
 kubectl delete deployment mythical-mysfits-eks 2> /dev/null
 kubectl delete service mythical-mysfits-eks 2> /dev/null
-eksctl delete iamserviceaccount \
-		--cluster=mythicaleks-eksctl \
-		--namespace=default \
-		--name=mythical-misfit
-export PolicyARNDynamoDB=$(aws iam list-policies --query 'Policies[?PolicyName==`MythicalMisfitDynamoDBTablePolicy`].Arn' --output text)
-aws iam delete-policy --policy-arn $PolicyARNDynamoDB
+#eksctl delete iamserviceaccount \
+#		--cluster=mythicaleks-eksctl \
+#		--namespace=default \
+#		--name=mythical-misfit
+#export PolicyARNDynamoDB=$(aws iam list-policies --query 'Policies[?PolicyName==`MythicalMisfitDynamoDBTablePolicy`].Arn' --output text)
+#aws iam delete-policy --policy-arn $PolicyARNDynamoDB
 
