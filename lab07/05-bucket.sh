7e8b4d947b3c3e0ac34b346025b3db5d90b7fe95
awsandy <awsandy@amazon.co.uk>
2023-09-06 21:24:31 +0100

    up

--- lab07/05-bucket.sh
+++ lab07/05-bucket.sh
@@ -1,7 +1,6 @@
 . ~/.bash_profile
 cd ~/environment/amazon-ecs-mythicalmysfits-workshop/workshop-1/app/monolith-service
-echo "bucket-name=${BUCKET_NAME}"
-aws s3 ls ${BUCKET_NAME}
+BUCKET_NAME="$(jq < ~/environment/amazon-ecs-mythicalmysfits-workshop/workshop-1/cfn-output.json -r '.SiteBucket')"
 aws s3 cp index.html s3://${BUCKET_NAME}/
 echo "hit http://${BUCKET_NAME}.s3-website.${AWS_REGION}.amazonaws.com/"
 curl -s http://${BUCKET_NAME}.s3-website.${AWS_REGION}.amazonaws.com/ | grep mysfits > /dev/null   
